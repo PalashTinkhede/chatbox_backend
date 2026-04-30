f4e841aece8bce43e824da44360101483e9963ec
PalashTinkhede <palash.tinkhede8124@gmail.com>
2026-04-30 22:20:13 +0530

    improved backend

--- constants/config.js
+++ constants/config.js
@@ -1,6 +1,6 @@
 const corsOptions = {
-  origin: "https://chat-box-client-iota.vercel.app",
-  
+  origin: "https://chat-box-frontend-coral.vercel.app",
+
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
--- controllers/chat.js
+++ controllers/chat.js
@@ -271,15 +271,17 @@ const sendAttachments = TryCatch(async (req, res, next) => {
 const getChatDetails = TryCatch(async (req, res, next) => {
   if (req.query.populate === "true") {
     const chat = await Chat.findById(req.params.id)
-      .populate("members", "name avatar")
+      .populate("members", "name avatar bio username")
       .lean();
 
     if (!chat) return next(new ErrorHandler("Chat not found", 404));
 
-    chat.members = chat.members.map(({ _id, name, avatar }) => ({
+    chat.members = chat.members.map(({ _id, name, avatar, bio, username }) => ({
       _id,
       name,
       avatar: avatar.url,
+      bio,
+      username,
     }));
 
     return res.status(200).json({
